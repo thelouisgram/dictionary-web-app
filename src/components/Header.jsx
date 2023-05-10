c6b17440532b1df9820dad013454ee3e843f719a
thelouisgram <hadesanoye01@gmail.com>
2023-05-10 22:12:59 +0100

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <AnimatePresence>
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 1}}
         transition={{ ease: "easeInOut", duration: 0.2 }}
       >
         <header className="flex w-full py-6 xs:py-8 relative sm:py-14 justify-between items-center">
