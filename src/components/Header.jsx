d1cd3356a5a3945fb300fd5152acb006504a4540
thelouisgram <hadesanoye01@gmail.com>
2023-05-10 22:11:46 +0100

    more

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,34 @@ import ThemeButton from "./headerComponents/ThemeButton";
 import Logo from "./headerComponents/Logo";
 import ThemeIcon from "./headerComponents/ThemeIcon";
 import FontFormContainer from "./headerComponents/fontFormComponents/FontFormContainer";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Header = () => {
   return (
     // Header Container
-    <header className="flex w-full py-6 xs:py-8 relative sm:py-14 justify-between items-center">
-      {/* Right Container & Logo */}
-      <Logo />
-      {/* Left Container */}
-      <div className="flex items-center gap-3 sm:gap-5">
-        {/* Font form and selection Dropdown menu container */}
-        <FontFormContainer />
-        {/* Barricade Div */}
-        <div className={`h-[30px] w-[1px] bg-[#e9e9e9]`} />
-        {/* Toggle Theme Container */}
-        <ThemeButton />
-        {/* Theme icon */}
-        <ThemeIcon />
-      </div>
-    </header>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 0.2 }}
+      >
+        <header className="flex w-full py-6 xs:py-8 relative sm:py-14 justify-between items-center">
+          {/* Right Container & Logo */}
+          <Logo />
+          {/* Left Container */}
+          <div className="flex items-center gap-3 sm:gap-5">
+            {/* Font form and selection Dropdown menu container */}
+            <FontFormContainer />
+            {/* Barricade Div */}
+            <div className={`h-[30px] w-[1px] bg-[#e9e9e9]`} />
+            {/* Toggle Theme Container */}
+            <ThemeButton />
+            {/* Theme icon */}
+            <ThemeIcon />
+          </div>
+        </header>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
